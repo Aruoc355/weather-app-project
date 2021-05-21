221a08bb468aebf5969fe2b8bcbf38c640171450
Alessia Ruocco <83032347+Aruoc355@users.noreply.github.com>
2021-05-21 17:32:25 +0000

    Initial commit
    uploaded all files for project

---
+++ index.js
@@ -0,0 +1,125 @@
+function showWeatherAtLocation(response) {
+  let h1 = document.querySelector("h1");
+  let definition = document.querySelector("#definition");
+  let localTemp = document.querySelector("#temperature");
+  let humidity = document.querySelector("#humidity-indicator");
+  let wind = document.querySelector("#wind");
+  let feelsLike = document.querySelector("#feels-like");
+  let feelsResult = Math.round(response.data.main.feels_like);
+  let windResult = Math.round(response.data.wind.speed);
+  let cityResult = response.data.name;
+  let weatherDescrip = response.data.weather[0].description;
+  let temperatureAtLocation = Math.round(response.data.main.temp);
+  let humAtLocation = response.data.main.humidity;
+  h1.innerHTML = cityResult;
+  definition.innerHTML = `${weatherDescrip} ,`;
+  localTemp.innerHTML = temperatureAtLocation;
+  humidity.innerHTML = `Humidity: ${humAtLocation}%`;
+  wind.innerHTML = `Wind: ${windResult}Km/h`;
+  feelsLike.innerHTML = `Feels like: ${feelsResult} °C`;
+}
+
+function getPosition(position) {
+  let yourLat = Math.round(position.coords.latitude);
+  let yourLon = Math.round(position.coords.longitude);
+  let apiKey = "86795cd1e54d46ec82adad776beccce9";
+  let locationUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${yourLat}&lon=${yourLon}&appid=${apiKey}&units=metric`;
+  axios.get(locationUrl).then(showWeatherAtLocation);
+}
+
+function getMyPosition(event) {
+  navigator.geolocation.getCurrentPosition(getPosition);
+}
+
+let myLocation = document.querySelector("#current-location");
+myLocation.addEventListener("click", getMyPosition);
+
+function displayTemp(response) {
+  let cityName = document.querySelector("h1");
+  let localTemp = document.querySelector("#temperature");
+  let definition = document.querySelector("#definition");
+  let humidity = document.querySelector("#humidity-indicator");
+  let wind = document.querySelector("#wind");
+  let feelsLike = document.querySelector("#feels-like");
+  let newCity = response.data.name;
+  let newHumidity = Math.round(response.data.main.humidity);
+  let windSpeed = Math.round(response.data.wind.speed);
+  let newFeelsLike = Math.round(response.data.main.feels_like);
+  let temp = Math.round(response.data.main.temp);
+  let weatherDef = response.data.weather[0].description;
+  cityName.innerHTML = `${newCity}`;
+  humidity.innerHTML = `Humidity: ${newHumidity}%`;
+  wind.innerHTML = `Wind: ${windSpeed} Km/h`;
+  feelsLike.innerHTML = `Feels like: ${newFeelsLike}°C`;
+  localTemp.innerHTML = temp;
+  definition.innerHTML = `${weatherDef}, `;
+}
+
+function search(city) {
+  let apiKey = "86795cd1e54d46ec82adad776beccce9";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemp);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#search-input").value;
+
+  search(cityInput);
+}
+
+let form = document.querySelector("form");
+
+form.addEventListener("submit", handleSubmit);
+
+let now = new Date();
+
+function getCurrentTime(Date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let day = days[now.getDay()];
+  let hour = now.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let date = now.getDate();
+  let month = months[now.getMonth()];
+
+  let updatedTime = `${day}, ${date} ${month}, ${hour}:${minutes}-`;
+  return updatedTime;
+}
+
+getCurrentTime();
+
+let dayTime = document.querySelector("#current-time");
+
+dayTime.innerHTML = getCurrentTime();
+
+search("Naples");
